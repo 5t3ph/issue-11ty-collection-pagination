1cfb35fa29cbfecaccb6f74f24f252ac9d1817ee
5t3ph <thinkdobecreate@gmail.com>
2022-06-04 17:15:13 -0500

    Issue: Unable to paginate a collection

---
+++ src/pages/page-one.11ty.js
@@ -0,0 +1,13 @@
+class PageOne {
+  data() {
+    return {
+      title: "Page One",
+    };
+  }
+
+  render() {
+    return;
+  }
+}
+
+module.exports = PageOne;
---
+++ src/pages/page-two.11ty.js
@@ -0,0 +1,13 @@
+class PageTwo {
+  data() {
+    return {
+      title: "Page Two",
+    };
+  }
+
+  render() {
+    return;
+  }
+}
+
+module.exports = PageTwo;
